38096015a3dc3b6ee561db9149ff25e9ce854a44
nitish <nitishk12c@gmail.com>
2020-12-25 07:10:33 +0530

    ds

--- binaryTree/practice/1.cpp
+++ binaryTree/practice/1.cpp
@@ -159,30 +159,183 @@ int count(node*root){
         return 0;
     return count(root->left) + count(root->right) + 1;
 }
+// sum of nodes
 int sum(node*root){
     if(root == null)
         return 0;
     return sum(root->left) + sum(root->right) + root->data;
 }
 
+int diameter(node*root){
+    if(root == null)
+        return 0;
+    
+    int h1 = height(root->left);
+    int h2 = height (root->right);
+
+    int op1 = h1+h2;
+    int op2 = diameter(root->left);
+    int op3 = diameter(root->right);
+
+    return max(op1, max(op1, op2));
+}
+
+class Pair{
+    public:
+        int height;
+        int diameter;
+};
+
+Pair diameter_optimized(node*root){
+    Pair p;
+    if(root==null){
+        p.diameter = p.height = 0;
+        return p;
+    }
+
+    Pair left = diameter_optimized(root->left);
+    Pair right = diameter_optimized(root->right);
+
+    p.height = max(left.height, right.height) + 1;
+    p.diameter = max(left.height + right.height, max(left.diameter, right.diameter));
+
+    return p;
+}
+
+
+// replace_nodes_with_sum_of_child_nodes
+
+int replaceNodeWithSum(node* &root){
+    if(root==null)
+        return 0;
+    if(root->left == null || root->right == null){
+        return root->data;
+    }
+
+    int leftSum = replaceNodeWithSum(root->left);
+    int rightSum = replaceNodeWithSum(root->right);
+
+    int temp = root->data;
+    root->data = leftSum + rightSum;
+    return temp + root->data;
+}
+
+
+
+
+// check height balanced tree
+class HBPair{
+    public:
+    int height;
+    bool balance;
+};
+
+HBPair isHeightBalance(node *root){
+    HBPair p;
+    if(root == null){
+        p.height = 0;
+        p.balance = true;
+        return p;
+    }
+
+    HBPair left = isHeightBalance(root->left);
+    HBPair right = isHeightBalance(root->right);
+
+    p.height = max(left.height, right.height) +1;
+
+    if(abs(left.height - right.height ) <=1 && left.balance && right.balance){
+        p.balance  = true;
+    }
+    else{
+        p.balance = false;
+    }
+
+    return p;
+}
+
+
+// build a height balanced tree
+node* buildTreeFromArray(int *a, int s , int e){
+    if(s > e)
+        return null;
+    
+    int mid  = (s+e)/2;
+    node*root =new node(a[mid]);
+    root->left = buildTreeFromArray(a, s, mid-1);
+    root->right = buildTreeFromArray(a, mid+1, e );
+
+    return root;
+}
+
+
+//  if we are given only pre order and inorder then binary tree will be unique
+node *createTreeFromTrav(int *in, int * pre,int s, int e ){
+    static int i = 0;
+    if(s>e){
+        return null;
+    }
+
+    node*root = new node(pre[i]);
+    int index = -1;
+
+    for(int j =s;j<=e;j++){
+        if(in[j] == pre[i]){
+            index = j;
+            break;
+        }
+    }
+    i++;
+    root->left = createTreeFromTrav(in, pre, s, index-1);
+    root->right = createTreeFromTrav(in, pre,index+1, e);
+
+    return root;
+}
+
 
 int main()
 {
 
-    node *root = buildTree();
-    print(root);
-    cout << endl;
-    in_print(root);
-    cout << endl;
-    post_print(root);
-    cout << endl;
-    cout << height(root) << endl;
-    int h = height(root);
-    print_all_level(root);
-    bfs(root);
-    cout << endl;
+    // node *root = buildTree();
+    // print(root);
+    // cout << endl;
+    // in_print(root);
+    // cout << endl;
+    // post_print(root);
+    // cout << endl;
+    // cout << height(root) << endl;
+    // int h = height(root);
+    // print_all_level(root);
+    // bfs(root);
+    // cout << endl;
+    // bfs_other(root);
+    // cout<<count(root)<<endl;
+    // cout<<sum(root)<<endl;
+    // cout<<diameter(root)<<endl;
+    // Pair p = diameter_optimized(root);
+    // cout<<p.height<<endl;
+    // cout<<p.diameter<<endl;
+    // replaceNodeWithSum(root);
+    // bfs_other(root);
+
+    // if(isHeightBalance(root).balance)
+    //     cout<<"true"<<endl;
+    // else{
+    //     cout<<"false"<<endl;
+    // }
+
+
+    // int a[]  = {1, 2, 3, 4,5 ,6, 7};
+    // int n = 7;
+    // node*root = buildTreeFromArray(a, 0, 6);
+    // bfs_other(root);
+
+
+    int in[] = {3,2,8, 4, 1, 6, 7 , 5};
+    int pre[] = {1,2,3,4,8, 5,6,7};
+    int n = sizeof(in)/sizeof(int);
+
+    node *root =createTreeFromTrav(in, pre, 0, n-1);
     bfs_other(root);
-    cout<<count(root)<<endl;
-    cout<<sum(root)<<endl;
+
     return 0;
 }
